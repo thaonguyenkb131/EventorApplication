ec377de134b50b30a5c67ec88499819b83fdd04d
PHAM HAI LONG <phamhailong.qn@gmail.com>
2025-06-17 10:22:00 +0700

    HL push main phần tui nha

---
+++ app/src/main/java/com/example/eventorapplication/Choose_gmail_popup.java
@@ -0,0 +1,20 @@
+package com.example.eventorapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Choose_gmail_popup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.choose_gmail_popup);
+
+    }
+}
---
+++ app/src/main/java/com/example/eventorapplication/PopupInterestFields.java
@@ -0,0 +1,19 @@
+package com.example.eventorapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PopupInterestFields extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.popup_interest_fields);
+    }
+}

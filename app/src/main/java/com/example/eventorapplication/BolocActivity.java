ebe73412a2e51d29537e503d0d843b45a6b53621
PTram <nptram0808@gmail.com>
2025-06-17 16:59:04 +0700

    update code

--- app/src/main/java/com/example/eventorapplication/BolocActivity.java
+++ app/src/main/java/com/example/eventorapplication/BolocActivity.java
@@ -1,24 +1,32 @@
 package com.example.eventorapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.eventorapplication.databinding.ActivityBolocBinding;
 
 public class BolocActivity extends AppCompatActivity {
+
+    private ActivityBolocBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_boloc);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityBolocBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.swMienphi.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                //Bật
+                binding.swMienphi.getThumbDrawable().setTint(Color.parseColor("#006183"));
+                binding.swMienphi.getTrackDrawable().setTint(Color.parseColor("#006183"));
+            } else {
+                // Tắt
+                binding.swMienphi.getThumbDrawable().setTint(Color.GRAY);
+                binding.swMienphi.getTrackDrawable().setTint(Color.LTGRAY);
+            }
         });
     }
 }
